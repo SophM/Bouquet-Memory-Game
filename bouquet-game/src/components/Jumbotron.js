6d6092aed1795fc5949c15f869caac1e6d00dacc
SophieM <47410186+SophM@users.noreply.github.com>
2019-04-23 23:50:53 -0700

    change picture for jumbotron background

--- bouquet-game/src/components/Jumbotron.js
+++ bouquet-game/src/components/Jumbotron.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Background from "../images/flowers.jpg";
+import Background from "../images/test-test.jpg";
 
 function Jumbotron() {
     return (
-        <div className="jumbotron jumbotron-fluid mb-0" style={{backgroundImage: "url(" + Background + ")", backgroundSize: "cover", backgroundPosition: "center"}}>
+        <div className="jumbotron jumbotron-fluid mb-0 pb-4" style={{backgroundImage: "url(" + Background + ")", backgroundSize: "cover", backgroundPosition: "bottom"}}>
             <div className="container text-center">
                 <h1 className="display-4 font-weight-bold text-white" style={{textShadow: "2px  2px 2px #3e8503, 2px  -2px 2px #3e8503, -2px  2px 2px #3e8503, -2px  -2px 2px #3e8503"}}>Bouquet - Memory Game</h1>
                 <h3 className="font-weight-bold text-white" style={{textShadow: "2px  2px 2px #3e8503, 2px  -2px 2px #3e8503, -2px  2px 2px #3e8503, -2px  -2px 2px #3e8503"}}>Pick flowers and make a bouquet</h3>
