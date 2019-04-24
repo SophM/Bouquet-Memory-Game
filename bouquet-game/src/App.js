bb1d497264deb5b1636726e7bd7f98b3c29bbc12
SophieM <47410186+SophM@users.noreply.github.com>
2019-04-24 16:07:44 -0700

    install the shuffle-array node package and code to App.js to shuffle the images on the page once one has been clicked

--- bouquet-game/src/App.js
+++ bouquet-game/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/NavBar";
 import Jumbotron from "./components/Jumbotron";
 import FlowerCards from "./components/FlowerCards";
 import flowers from "./flowers.json";
+import Shuffle from "shuffle-array";
 
 class App extends Component {
 
@@ -10,9 +11,10 @@ class App extends Component {
     flowers,
   }
 
-
-
-
+  handleClickImage = () => {
+    console.log("test");
+    this.setState(Shuffle(this.state.flowers));
+  }
 
   render() {
     return (
@@ -22,9 +24,11 @@ class App extends Component {
         <div className="container mt-5 mb-5 text-center">
           {this.state.flowers.map(flower => (
             <FlowerCards
-              id={flower.id} 
+              id={flower.id}
+              key={flower.id} 
               name={flower.name}
               image={flower.image}
+              handleClickImage={this.handleClickImage}
             />
           ))}
         </div>  
--- bouquet-game/src/components/FlowerCards.js
+++ bouquet-game/src/components/FlowerCards.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function FlowerCard(props) {
     return (
-        <img className="img-fluid img-thumbnail m-3" src={props.image} alt={props.name} data-id={props.id} style={{width: "150px", height: "150px"}}/>
+        <img className="img-fluid img-thumbnail m-3" type="button" src={props.image} alt={props.name} data-id={props.id} onClick={() => props.handleClickImage()} style={{width: "150px", height: "150px"}}/>
     );
 }
 
