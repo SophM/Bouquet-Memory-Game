84e4f732b99f449af957422c2e4aef3e56b067f9
SophieM <47410186+SophM@users.noreply.github.com>
2019-04-23 23:30:58 -0700

    add FlowerCard component, flowers.json with flower data and code in App.js to render the flowers on the page

--- bouquet-game/src/App.js
+++ bouquet-game/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import Navbar from "./components/NavBar";
 import Jumbotron from "./components/Jumbotron";
+import FlowerCards from "./components/FlowerCards";
+import flowers from "./flowers.json";
 
 class App extends Component {
+
+  state = {
+    flowers,
+  }
 
 
 
@@ -13,6 +19,15 @@ class App extends Component {
       <div>
         <Jumbotron />
         <Navbar />
+        <div className="container mt-5 mb-5 text-center">
+          {this.state.flowers.map(flower => (
+            <FlowerCards
+              id={flower.id} 
+              name={flower.name}
+              image={flower.image}
+            />
+          ))}
+        </div>  
       </div>
     );
   }
