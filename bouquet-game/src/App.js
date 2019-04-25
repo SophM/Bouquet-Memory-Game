cde955fa407c900bfdabebef974e818fed427216
SophieM <47410186+SophM@users.noreply.github.com>
2019-04-24 23:03:42 -0700

    add logic for updating score/top score and add message to be displayed

--- bouquet-game/src/App.js
+++ bouquet-game/src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
   state = {
     flowers,
     idFlowersClicked: [],
+    message: "Ready? Click a flower to begin!",
     score : 0,
     topScore: 0
   }
@@ -18,15 +19,43 @@ class App extends Component {
     // console.log("test");
     const flowerClicked = event.target.attributes.getNamedItem("data-id").value;
     // console.log(flowerClicked);
+    // console.log(this.state.idFlowersClicked.includes(flowerClicked))
 
-    this.setState(Shuffle(this.state.flowers));
+    // clone this.state to a new object that will be modified
+    const newState = {...this.state};
+
+    // if the flower hasn't already been clicked
+    if (!newState.idFlowersClicked.includes(flowerClicked)) {
+      // push the id of the flower clicked into the array idFlowersClicked
+      newState.idFlowersClicked.push(flowerClicked);
+      // increment the score
+      newState.score = newState.score + 1;
+      // increment the topScore if it is equal to score
+      newState.topScore = ((newState.topScore + 1) === newState.score) ? (newState.topScore + 1) : newState.topScore;
+      // modify the message the message displayed
+      newState.message = "Great! New flower added!"
+    // if the flower has already been clicked
+    } else {
+      // modify the message the message displayed
+      newState.message = "Oh no! This one was already in there...!"
+      // reset the score to 0
+      newState.score = 0;
+      // empty the array idFlowersClicked
+      newState.idFlowersClicked = [];
+    }
+    
+    // replace the state object with the newState one
+    this.setState(newState);
+    // shuffle the flower pictures on the page
+    this.setState(Shuffle(this.state.flowers));  
   }
 
   render() {
     return (
       <div>
         <Jumbotron />
-        <Navbar 
+        <Navbar
+          message={this.state.message}
           score={this.state.score}
           topScore={this.state.topScore}
         />
--- bouquet-game/src/components/NavBar.js
+++ bouquet-game/src/components/NavBar.js
@@ -3,6 +3,7 @@ import React from "react";
 function NavBar(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: "#3e8503"}}>
+            <h3 className="text-white ml-auto">{props.message}</h3>
             <h3 className="text-white mx-auto">Score: {props.score} | Top Score: {props.topScore}</h3>
         </nav>
     );
